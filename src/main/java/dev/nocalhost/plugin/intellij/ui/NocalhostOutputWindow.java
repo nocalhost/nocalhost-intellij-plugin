0afa40d888aa77754623707159ce9d376baea918
mingshun <huangmingxin@coding.net>
2021-02-03 17:21:37 +0800

    feat: add clear and scroll to end actions to nocalhost output window

--- src/main/java/dev/nocalhost/plugin/intellij/ui/NocalhostOutputWindow.java
+++ src/main/java/dev/nocalhost/plugin/intellij/ui/NocalhostOutputWindow.java
@@ -1,11 +1,24 @@
 package dev.nocalhost.plugin.intellij.ui;
 
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.ActionToolbar;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.actionSystem.ToggleAction;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.components.JBTextArea;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.swing.*;
 
@@ -15,22 +28,29 @@ public class NocalhostOutputWindow {
     private final Project project;
     private final ToolWindow toolWindow;
 
-    private JPanel panel;
+    private NocalhostOutputWindowPanel panel;
     private JBTextArea textArea;
     private JBScrollPane scrollPane;
+
+    private AtomicBoolean scrollToEnd = new AtomicBoolean(true);
 
     public NocalhostOutputWindow(Project project, ToolWindow toolWindow) {
         this.project = project;
         this.toolWindow = toolWindow;
 
-        panel = new SimpleToolWindowPanel(true);
+        panel = new NocalhostOutputWindowPanel(false);
         textArea = new JBTextArea();
         textArea.setEditable(false);
         textArea.setLineWrap(true);
-        textArea.setAutoscrolls(true);
         scrollPane = new JBScrollPane(textArea);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         panel.add(scrollPane);
+
+        DefaultActionGroup actionGroup = new DefaultActionGroup();
+        actionGroup.add(new ClearAction());
+        actionGroup.add(new ScrollToEndAction());
+        ActionToolbar actionToolbar = ActionManager.getInstance().createActionToolbar("Nocalhost.Output.Window.Toolbar", actionGroup, false);
+        panel.setToolbar(actionToolbar.getComponent());
 
         project.getMessageBus().connect().subscribe(
                 NocalhostOutputAppendNotifier.NOCALHOST_OUTPUT_APPEND_NOTIFIER,
@@ -41,13 +61,85 @@ public class NocalhostOutputWindow {
 
     private void appendOutput(String text) {
         ApplicationManager.getApplication().invokeAndWait(() -> {
+            int currentPos = textArea.getCaretPosition();
             textArea.append(text);
-            int length = textArea.getDocument().getLength();
-            textArea.select(length, length);
+            if (scrollToEnd.get()) {
+                JScrollBar vertical = scrollPane.getVerticalScrollBar();
+                vertical.setValue(vertical.getMaximum());
+            } else {
+                textArea.select(currentPos, currentPos);
+            }
         });
     }
 
     public JPanel getPanel() {
         return panel;
+    }
+
+    private class ClearAction extends DumbAwareAction {
+        private ClearAction() {
+            super("Clear", "Clear", AllIcons.Actions.GC);
+        }
+
+        @Override
+        public void update(@NotNull AnActionEvent e) {
+            e.getPresentation().setEnabled(true);
+        }
+
+        @Override
+        public void actionPerformed(@NotNull AnActionEvent e) {
+            textArea.setText("");
+        }
+    }
+
+    private class ScrollToEndAction extends ToggleAction {
+        private ScrollToEndAction() {
+            super("Scroll to End", "Scroll to End", AllIcons.RunConfigurations.Scroll_down);
+        }
+
+        @Override
+        public boolean isSelected(@NotNull AnActionEvent e) {
+            return scrollToEnd.get();
+        }
+
+        @Override
+        public void setSelected(@NotNull AnActionEvent e, boolean state) {
+            scrollToEnd.set(state);
+            if (state) {
+                JScrollBar vertical = scrollPane.getVerticalScrollBar();
+                vertical.setValue(vertical.getMaximum());
+            }
+        }
+    }
+
+    private class NocalhostOutputWindowPanel extends SimpleToolWindowPanel {
+
+        public NocalhostOutputWindowPanel(boolean vertical) {
+            super(vertical);
+        }
+
+        @Override
+        public void setToolbar(@Nullable JComponent c) {
+            Component myToolbar = super.getToolbar();
+            if (c == null) {
+                remove(myToolbar);
+            }
+            myToolbar = c;
+            super.setToolbar(c);
+            if (myToolbar instanceof ActionToolbar) {
+                ((ActionToolbar) myToolbar).setOrientation(myVertical ? SwingConstants.HORIZONTAL : SwingConstants.VERTICAL);
+            }
+
+            if (c != null) {
+                if (myVertical) {
+                    add(c, BorderLayout.NORTH);
+                } else {
+                    add(c, BorderLayout.EAST);
+                }
+            }
+
+            revalidate();
+            repaint();
+        }
     }
 }
