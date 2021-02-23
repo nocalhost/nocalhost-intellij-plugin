4fc0f5ff16c47c9f0ba4b0324ded2cd44fbafe8c
mingshun <huangmingxin@coding.net>
2021-02-23 15:54:32 +0800

    fix: output window would not activate when nhctl command executes

--- src/main/java/dev/nocalhost/plugin/intellij/commands/OutputCapturedNhctlCommand.java
+++ src/main/java/dev/nocalhost/plugin/intellij/commands/OutputCapturedNhctlCommand.java
@@ -4,7 +4,13 @@ import com.google.common.base.Charsets;
 
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.serviceContainer.AlreadyDisposedException;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -13,12 +19,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
+import dev.nocalhost.plugin.intellij.NocalhostNotifier;
 import dev.nocalhost.plugin.intellij.commands.data.NhctlGlobalOptions;
 import dev.nocalhost.plugin.intellij.exception.NocalhostExecuteCmdException;
-import dev.nocalhost.plugin.intellij.topic.NocalhostOutputActivateNotifier;
 import dev.nocalhost.plugin.intellij.topic.NocalhostOutputAppendNotifier;
 
 public final class OutputCapturedNhctlCommand extends NhctlCommand {
+    private static final Logger LOG = Logger.getInstance(OutputCapturedNhctlCommand.class);
+
     private final Project project;
 
     public OutputCapturedNhctlCommand(Project project) {
@@ -29,9 +37,7 @@ public final class OutputCapturedNhctlCommand extends NhctlCommand {
     protected String execute(List<String> args, NhctlGlobalOptions opts) throws IOException, InterruptedException, NocalhostExecuteCmdException {
         addGlobalOptions(args, opts);
 
-        project.getMessageBus()
-                .syncPublisher(NocalhostOutputActivateNotifier.NOCALHOST_OUTPUT_ACTIVATE_NOTIFIER_TOPIC)
-                .action();
+        activateOutputWindow();
 
         NocalhostOutputAppendNotifier publisher = project.getMessageBus()
                 .syncPublisher(NocalhostOutputAppendNotifier.NOCALHOST_OUTPUT_APPEND_NOTIFIER_TOPIC);
@@ -67,5 +73,24 @@ public final class OutputCapturedNhctlCommand extends NhctlCommand {
         }
 
         return sb.toString();
+    }
+
+    private void activateOutputWindow() {
+        ApplicationManager.getApplication().invokeAndWait(() -> {
+            try {
+                ToolWindowManager.getInstance(project).getToolWindow("Nocalhost Console").activate(() -> {
+                    ContentManager contentManager = ToolWindowManager.getInstance(project).getToolWindow("Nocalhost Console").getContentManager();
+                    Content content = contentManager.getContent(0);
+                    if (content != null) {
+                        contentManager.setSelectedContent(content);
+                    }
+                });
+            } catch (AlreadyDisposedException e) {
+                // Ignore
+            } catch (Exception e) {
+                LOG.error("error occurred while activate output window", e);
+                NocalhostNotifier.getInstance(project).notifyError("Nocalhost starting dev mode error", "Error occurred while starting dev mode", e.getMessage());
+            }
+        });
     }
 }
--- src/main/java/dev/nocalhost/plugin/intellij/startup/NocalhostStartupActivity.java
+++ src/main/java/dev/nocalhost/plugin/intellij/startup/NocalhostStartupActivity.java
@@ -1,15 +1,10 @@
 package dev.nocalhost.plugin.intellij.startup;
 
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.serviceContainer.AlreadyDisposedException;
-import com.intellij.ui.content.Content;
-import com.intellij.ui.content.ContentManager;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -21,26 +16,16 @@ import dev.nocalhost.plugin.intellij.api.data.DevModeService;
 import dev.nocalhost.plugin.intellij.api.data.DevSpace;
 import dev.nocalhost.plugin.intellij.settings.NocalhostSettings;
 import dev.nocalhost.plugin.intellij.task.StartingDevModeTask;
-import dev.nocalhost.plugin.intellij.topic.NocalhostOutputActivateNotifier;
 
 public final class NocalhostStartupActivity implements StartupActivity {
     private static final Logger LOG = Logger.getInstance(NocalhostStartupActivity.class);
 
-    private Project project;
-
     @Override
     public void runActivity(@NotNull Project project) {
-        this.project = project;
-
-        project.getMessageBus().connect().subscribe(
-                NocalhostOutputActivateNotifier.NOCALHOST_OUTPUT_ACTIVATE_NOTIFIER_TOPIC,
-                this::activateOutput
-        );
-
-        devStart();
+        devStart(project);
     }
 
-    private void devStart() {
+    private void devStart(Project project) {
         final NocalhostSettings nocalhostSettings = ServiceManager.getService(NocalhostSettings.class);
         DevModeService devModeService = nocalhostSettings.getDevModeProjectBasePath2Service().get(project.getBasePath());
         if (nocalhostSettings.getUserInfo() != null && devModeService != null) {
@@ -62,22 +47,4 @@ public final class NocalhostStartupActivity implements StartupActivity {
         }
     }
 
-    private void activateOutput() {
-        ApplicationManager.getApplication().invokeAndWait(() -> {
-            try {
-                ToolWindowManager.getInstance(project).getToolWindow("Nocalhost Console").activate(() -> {
-                    ContentManager contentManager = ToolWindowManager.getInstance(project).getToolWindow("Nocalhost Console").getContentManager();
-                    Content content = contentManager.getContent(0);
-                    if (content != null) {
-                        contentManager.setSelectedContent(content);
-                    }
-                });
-            } catch (AlreadyDisposedException e) {
-                // Ignore
-            } catch (Exception e) {
-                LOG.error("error occurred while activate output window", e);
-                NocalhostNotifier.getInstance(project).notifyError("Nocalhost starting dev mode error", "Error occurred while starting dev mode", e.getMessage());
-            }
-        });
-    }
 }
