9ac4cbf6a7443c71789cb15d7555339c2d182594
huangmingxin <huangmingxin@coding.net>
2021-04-13 18:00:20 +0800

    fix: ui display and operation logic

--- src/main/java/dev/nocalhost/plugin/intellij/ui/InstallApplicationChooseDialog.java
+++ src/main/java/dev/nocalhost/plugin/intellij/ui/InstallApplicationChooseDialog.java
@@ -6,6 +6,8 @@ import com.intellij.ui.components.JBList;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
 
 import javax.swing.*;
@@ -26,6 +28,15 @@ public class InstallApplicationChooseDialog extends DialogWrapper {
         applicationList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         applicationList.setListData(apps.toArray(new String[0]));
         applicationList.setSelectedIndex(0);
+        applicationList.setVisibleRowCount(15);
+        applicationList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent event) {
+                if (event.getClickCount() == 2 && event.getButton() == MouseEvent.BUTTON1) {
+                    doOKAction();
+                }
+            }
+        });
 
         init();
     }
