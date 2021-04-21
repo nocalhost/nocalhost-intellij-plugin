992c5a5d19ede239bb0d44be59f559b9a3305b86
huangmingxin <huangmingxin@coding.net>
2021-04-21 10:34:42 +0800

    fix: deprecated api to remove related reports

--- src/main/java/dev/nocalhost/plugin/intellij/configuration/java/NocalhostJavaConfigurationFactory.java
+++ src/main/java/dev/nocalhost/plugin/intellij/configuration/java/NocalhostJavaConfigurationFactory.java
@@ -5,6 +5,7 @@ import com.intellij.execution.configurations.ConfigurationType;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.openapi.project.Project;
 
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 public class NocalhostJavaConfigurationFactory extends ConfigurationFactory {
@@ -16,5 +17,12 @@ public class NocalhostJavaConfigurationFactory extends ConfigurationFactory {
     @Override
     public @NotNull RunConfiguration createTemplateConfiguration(@NotNull Project project) {
         return new NocalhostJavaConfiguration(project, this);
+    }
+
+    @Override
+    @NotNull
+    @NonNls
+    public String getId() {
+        return "NocalhostJavaConfigurationFactory";
     }
 }
