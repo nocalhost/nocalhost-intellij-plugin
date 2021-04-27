799361e9f27e3dc95cdad60c228a12f0357ea8cc
huangmingxin <huangmingxin@coding.net>
2021-04-27 17:07:07 +0800

    fix: git get remote command assert failure

--- src/main/java/dev/nocalhost/plugin/intellij/ui/action/workload/StartDevelopAction.java
+++ src/main/java/dev/nocalhost/plugin/intellij/ui/action/workload/StartDevelopAction.java
@@ -1,8 +1,11 @@
 package dev.nocalhost.plugin.intellij.ui.action.workload;
 
+import com.google.common.collect.Lists;
+
 import com.intellij.ide.impl.OpenProjectTask;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressIndicator;
@@ -47,6 +50,11 @@ import icons.NocalhostIcons;
 public class StartDevelopAction extends AnAction {
     private static final Logger LOG = Logger.getInstance(StartDevelopAction.class);
 
+    private final NhctlCommand nhctlCommand = ServiceManager.getService(NhctlCommand.class);
+    private final KubectlCommand kubectlCommand = ServiceManager.getService(KubectlCommand.class);
+    private final GitCommand gitCommand = ServiceManager.getService(GitCommand.class);
+    private final NocalhostSettings nocalhostSettings = ServiceManager.getService(NocalhostSettings.class);
+
     private final Project project;
     private final ResourceNode node;
 
@@ -57,15 +65,11 @@ public class StartDevelopAction extends AnAction {
     }
 
     private String selectContainer(List<String> containers) {
-        if (containers.size() > 1) {
-            StartDevelopContainerChooseDialog dialog = new StartDevelopContainerChooseDialog(containers);
-            if (dialog.showAndGet()) {
-                return dialog.getSelectedContainer();
-            } else {
-                return null;
-            }
+        StartDevelopContainerChooseDialog dialog = new StartDevelopContainerChooseDialog(containers);
+        if (dialog.showAndGet()) {
+            return dialog.getSelectedContainer();
         } else {
-            return containers.get(0);
+            return null;
         }
     }
 
@@ -81,154 +85,194 @@ public class StartDevelopAction extends AnAction {
         return null;
     }
 
-    @Override
-    public void actionPerformed(@NotNull AnActionEvent event) {
-        NhctlDescribeService nhctlDescribeService;
-        String startDevelopContainerName = "";
+    private Optional<NhctlDescribeService> getNhctlDescribeService() {
         try {
-            final NhctlCommand nhctlCommand = ServiceManager.getService(NhctlCommand.class);
             NhctlDescribeOptions opts = new NhctlDescribeOptions(node.devSpace());
             opts.setDeployment(node.resourceName());
-            nhctlDescribeService = nhctlCommand.describe(
-                    node.applicationName(),
-                    opts,
-                    NhctlDescribeService.class);
-            if (nhctlDescribeService.isDeveloping()) {
-                Messages.showMessageDialog("Dev mode has been started.", "Start Develop", null);
+            NhctlDescribeService nhctlDescribeService = nhctlCommand.describe(
+                    node.applicationName(), opts, NhctlDescribeService.class);
+            return Optional.of(nhctlDescribeService);
+        } catch (IOException | InterruptedException | NocalhostExecuteCmdException e) {
+            LOG.error("error occurred while checking if service was in development", e);
+        }
+        return Optional.empty();
+    }
+
+    private List<KubeResource> getDeploymentPods() {
+        try {
+            KubeResource deployment = kubectlCommand.getResource("deployment",
+                    node.resourceName(), node.devSpace());
+            KubeResourceList pods = kubectlCommand.getResourceList("pods",
+                    deployment.getSpec().getSelector().getMatchLabels(), node.devSpace());
+            return pods.getItems().stream()
+                    .filter(KubeResource::canSelector)
+                    .collect(Collectors.toList());
+        } catch (IOException | InterruptedException | NocalhostExecuteCmdException e) {
+            LOG.error("error occurred while listing deployment pods", e);
+        }
+        return Lists.newArrayList();
+    }
+
+    private void selectSourceDirectory(final String gitUrl, final DevModeService devModeService) {
+        ApplicationManager.getApplication().invokeLater(() -> {
+            int exitCode = MessageDialogBuilder
+                    .yesNoCancel(
+                            "Start develop",
+                            "To start develop, you must specify source code directory."
+                    )
+                    .yesText("Clone from Git Repo")
+                    .noText("Open local directly")
+                    .guessWindowAndAsk();
+
+            switch (exitCode) {
+                case Messages.YES: {
+                    if (!StringUtils.isNotEmpty(gitUrl)) {
+                        Messages.showMessageDialog("Git url not found", "Clone Repo", null);
+                        return;
+                    }
+
+                    final Path parentDir = FileChooseUtil.chooseSingleDirectory(project);
+                    if (parentDir == null) {
+                        return;
+                    }
+
+                    ProgressManager.getInstance().run(new Task.Backgroundable(null,
+                            "Cloning " + gitUrl, false) {
+                        private Path gitDir;
+
+                        @Override
+                        public void onSuccess() {
+                            super.onSuccess();
+                            ProjectManagerEx.getInstanceEx().openProject(gitDir,
+                                    new OpenProjectTask());
+                        }
+
+                        @Override
+                        public void run(@NotNull ProgressIndicator indicator) {
+                            try {
+                                gitCommand.clone(parentDir, gitUrl, node.resourceName(), project);
+
+                                gitDir = parentDir.resolve(node.resourceName());
+
+                                nocalhostSettings.getDevModeProjectBasePath2Service().put(
+                                        gitDir.toString(),
+                                        devModeService
+                                );
+                            } catch (NocalhostGitException e) {
+                                LOG.error("error occurred while cloning git repository", e);
+                                NocalhostNotifier.getInstance(project).notifyError(
+                                        "Nocalhost clone repository error",
+                                        "Error occurred while clone repository",
+                                        e.getMessage());
+                            }
+                        }
+                    });
+                }
+
+                break;
+                case Messages.NO: {
+                    final Path basePath = FileChooseUtil.chooseSingleDirectory(project);
+                    if (basePath == null) {
+                        return;
+                    }
+
+                    nocalhostSettings.getDevModeProjectBasePath2Service().put(
+                            basePath.toString(),
+                            devModeService
+                    );
+
+                    ProjectManagerEx.getInstanceEx().openProject(basePath, new OpenProjectTask());
+                }
+                break;
+                default:
+            }
+        });
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        ApplicationManager.getApplication().executeOnPooledThread(() -> {
+            Optional<NhctlDescribeService> nhctlDescribeServiceOptional = getNhctlDescribeService();
+            if (nhctlDescribeServiceOptional.isEmpty()) {
                 return;
             }
 
-            final KubectlCommand kubectlCommand = ServiceManager.getService(KubectlCommand.class);
-            KubeResource deployment = kubectlCommand.getResource("deployment", node.resourceName(), node.devSpace());
-            KubeResourceList pods = kubectlCommand.getResourceList("pods", deployment.getSpec().getSelector().getMatchLabels(), node.devSpace());
-            if (pods.getItems().get(0).getSpec().getContainers().size() > 1) {
-                final List<KubeResource> running = pods
-                        .getItems()
-                        .stream()
-                        .filter(KubeResource::canSelector)
-                        .collect(Collectors.toList());
-                if (running.size() > 0) {
-                    startDevelopContainerName = selectContainer(running
+            NhctlDescribeService nhctlDescribeService = nhctlDescribeServiceOptional.get();
+            if (nhctlDescribeService.isDeveloping()) {
+                ApplicationManager.getApplication().invokeLater(() -> {
+                    Messages.showMessageDialog("Dev mode has been started.",
+                            "Start Develop", null);
+                });
+                return;
+            }
+
+            List<KubeResource> pods = getDeploymentPods();
+            ApplicationManager.getApplication().invokeLater(() -> {
+                String startDevelopContainerName = "";
+                if (pods.size() > 0 && pods.get(0).getSpec().getContainers().size() > 1) {
+                    startDevelopContainerName = selectContainer(pods
                             .get(0)
                             .getSpec()
                             .getContainers()
                             .stream()
                             .map(KubeResource.Spec.Container::getName)
                             .collect(Collectors.toList()));
-                }
-                if (!StringUtils.isNotEmpty(startDevelopContainerName)) {
-                    return;
-                }
-            }
-        } catch (IOException | InterruptedException | NocalhostExecuteCmdException e) {
-            LOG.error("error occurred while checking if service was in development", e);
-            return;
-        }
-
-        final String containerName = startDevelopContainerName;
-
-        final String gitUrl = findGitUrl(nhctlDescribeService.getRawConfig().getContainers(), containerName);
-
-        DevModeService devModeService = new DevModeService(node.applicationName(), node.devSpace().getId(), node.resourceName(), containerName);
-
-        final String path = project.getBasePath();
-
-        final NocalhostSettings nocalhostSettings = ServiceManager.getService(NocalhostSettings.class);
-        final Optional<NocalhostRepo> nocalhostRepo = nocalhostSettings.getRepos().stream()
-                                                                       .filter(repos -> repos.getHost().equals(nocalhostSettings.getBaseUrl())
-                                                                                && repos.getEmail().equals(nocalhostSettings.getUserInfo().getEmail())
-                                                                                && repos.getAppName().equals(node.applicationName())
-                                                                                && Objects.equals(repos.getDevSpaceId(), devModeService.getDevSpaceId())
-                                                                                && repos.getDeploymentName().equals(devModeService.getServiceName())).findFirst();
-
-        if (nocalhostRepo.isPresent()) {
-            final NocalhostRepo repo = nocalhostRepo.get();
-            if (repo.getRepoPath().equals(project.getBasePath())) {
-                ProgressManager.getInstance().run(new StartingDevModeTask(project, node.devSpace(), node.applicationName(), devModeService));
-            } else {
-                nocalhostSettings.getDevModeProjectBasePath2Service().put(
-                        repo.getRepoPath(),
-                        devModeService
-                );
-
-                ProjectManagerEx.getInstanceEx().openProject(Path.of(repo.getRepoPath()), new OpenProjectTask());
-            }
-            return;
-        }
-
-        final GitCommand gitCommand = ServiceManager.getService(GitCommand.class);
-
-        try {
-            String ps = gitCommand.getRemote(path, project);
-            final Optional<String> optionalPath = Arrays.stream(ps.split("\n")).map(p -> p.split("\t")[1].split(" ")[0]).filter(p -> p.equals(gitUrl)).findFirst();
-            if (optionalPath.isPresent()) {
-                ProgressManager.getInstance().run(new StartingDevModeTask(project, node.devSpace(), node.applicationName(), devModeService));
-                return;
-            }
-        } catch (Exception ignored) {
-        }
-
-        int exitCode = MessageDialogBuilder.yesNoCancel("Start develop", "To start develop, you must specify source code directory.")
-                .yesText("Clone from Git Repo")
-                .noText("Open local directly")
-                .guessWindowAndAsk();
-
-        switch (exitCode) {
-            case Messages.YES: {
-                if (!StringUtils.isNotEmpty(gitUrl)) {
-                    Messages.showMessageDialog("Git url not found", "Clone Repo", null);
-                    return;
-                }
-
-                final Path parentDir = FileChooseUtil.chooseSingleDirectory(project);
-                if (parentDir == null) {
-                    return;
-                }
-
-                ProgressManager.getInstance().run(new Task.Backgroundable(null, "Cloning " + gitUrl, false) {
-                    private Path gitDir;
-
-                    @Override
-                    public void onSuccess() {
-                        super.onSuccess();
-                        ProjectManagerEx.getInstanceEx().openProject(gitDir, new OpenProjectTask());
+                    if (!StringUtils.isNotEmpty(startDevelopContainerName)) {
+                        return;
                     }
+                }
 
-                    @Override
-                    public void run(@NotNull ProgressIndicator indicator) {
-                        try {
-                            gitCommand.clone(parentDir, gitUrl, node.resourceName(), project);
+                final String containerName = startDevelopContainerName;
 
-                            gitDir = parentDir.resolve(node.resourceName());
+                String gitUrl = findGitUrl(nhctlDescribeService.getRawConfig().getContainers(),
+                        containerName);
 
-                            nocalhostSettings.getDevModeProjectBasePath2Service().put(
-                                    gitDir.toString(),
-                                    devModeService
-                            );
-                        } catch (NocalhostGitException e) {
-                            LOG.error("error occurred while cloning git repository", e);
-                            NocalhostNotifier.getInstance(project).notifyError("Nocalhost clone repository error", "Error occurred while clone repository", e.getMessage());
+                DevModeService devModeService = new DevModeService(node.applicationName(),
+                        node.devSpace().getId(), node.resourceName(), containerName);
+
+                final Optional<NocalhostRepo> nocalhostRepo = nocalhostSettings.getRepos().stream()
+                        .filter(repos -> repos.getHost().equals(nocalhostSettings.getBaseUrl())
+                                && repos.getEmail().equals(nocalhostSettings.getUserInfo().getEmail())
+                                && repos.getAppName().equals(node.applicationName())
+                                && Objects.equals(repos.getDevSpaceId(), devModeService.getDevSpaceId())
+                                && repos.getDeploymentName().equals(devModeService.getServiceName())).findFirst();
+
+                if (nocalhostRepo.isPresent()) {
+                    final NocalhostRepo repo = nocalhostRepo.get();
+                    if (repo.getRepoPath().equals(project.getBasePath())) {
+                        ProgressManager.getInstance().run(new StartingDevModeTask(project,
+                                node.devSpace(), node.applicationName(), devModeService));
+                    } else {
+                        nocalhostSettings.getDevModeProjectBasePath2Service().put(
+                                repo.getRepoPath(),
+                                devModeService
+                        );
+
+                        ProjectManagerEx.getInstanceEx().openProject(Path.of(repo.getRepoPath()),
+                                new OpenProjectTask());
+                    }
+                    return;
+                }
+
+                ApplicationManager.getApplication().executeOnPooledThread(() -> {
+                    try {
+                        String ps = gitCommand.getRemote(project.getBasePath(), project);
+                        final Optional<String> optionalPath = Arrays.stream(ps.split("\n"))
+                                .map(p -> p.split("\t")[1].split(" ")[0])
+                                .filter(p -> p.equals(gitUrl)).findFirst();
+                        if (optionalPath.isPresent()) {
+                            ApplicationManager.getApplication().invokeLater(() -> {
+                                ProgressManager.getInstance().run(new StartingDevModeTask(project,
+                                        node.devSpace(), node.applicationName(), devModeService));
+                            });
+                            return;
                         }
+                    } catch (Exception ignored) {
                     }
+
+                    selectSourceDirectory(gitUrl, devModeService);
                 });
-            }
-
-            break;
-            case Messages.NO: {
-                final Path basePath = FileChooseUtil.chooseSingleDirectory(project);
-                if (basePath == null) {
-                    return;
-                }
-
-                nocalhostSettings.getDevModeProjectBasePath2Service().put(
-                        basePath.toString(),
-                        devModeService
-                );
-
-                ProjectManagerEx.getInstanceEx().openProject(basePath, new OpenProjectTask());
-            }
-            break;
-            default:
-        }
+            });
+        });
     }
 }
