1610726fdc814b30eb552ec8de657914c3d9d9b4
huangmingxin <huangmingxin@coding.net>
2021-05-17 11:06:06 +0800

    fix: nhctl describe yaml parse error

--- src/main/java/dev/nocalhost/plugin/intellij/commands/data/ServiceContainerDev.java
+++ src/main/java/dev/nocalhost/plugin/intellij/commands/data/ServiceContainerDev.java
@@ -12,13 +12,8 @@ public class ServiceContainerDev {
     private String image;
     private String shell;
     private String workDir;
-    private Object resources;
-    private List<Object> persistentVolumeDirs;
     private ServiceContainerDevCommand command;
     private ServiceContainerDevDebug debug;
     private boolean useDevContainer;
-    private Object sync;
-    private List<Object> env;
-    private List<Object> envFrom;
     private List<String> portForward;
 }
