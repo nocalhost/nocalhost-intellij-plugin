83227cd3cdd63a7ca48e56e05a16de495ed1780b
huangmingxin <huangmingxin@coding.net>
2021-06-24 17:11:30 +0800

    refactor: replace workload types with constants

--- src/main/java/dev/nocalhost/plugin/intellij/utils/Constants.java
+++ src/main/java/dev/nocalhost/plugin/intellij/utils/Constants.java
@@ -1,5 +1,9 @@
 package dev.nocalhost.plugin.intellij.utils;
 
+import com.google.common.collect.ImmutableSet;
+
+import java.util.Set;
+
 public class Constants {
 
     public static final String DEFAULT_APPLICATION_NAME = "default.application";
@@ -13,5 +17,20 @@ public class Constants {
     public static final String MANIFEST_TYPE_KUSTOMIZE_GIT = "kustomizeGit";
     public static final String MANIFEST_TYPE_KUSTOMIZE_LOCAL = "kustomizeLocal";
 
+    public static final String WORKLOAD_TYPE_DEPLOYMENT = "deployment";
+    public static final String WORKLOAD_TYPE_STATEFULSET = "statefulset";
+    public static final String WORKLOAD_TYPE_DAEMONSET = "daemonset";
+    public static final String WORKLOAD_TYPE_JOB = "job";
+    public static final String WORKLOAD_TYPE_CRONJOB = "cronjob";
+    public static final String WORKLOAD_TYPE_POD = "pod";
+
+    public static final ImmutableSet<String> ALL_WORKLOAD_TYPES = ImmutableSet.of(
+            WORKLOAD_TYPE_DEPLOYMENT,
+            WORKLOAD_TYPE_STATEFULSET,
+            WORKLOAD_TYPE_DAEMONSET,
+            WORKLOAD_TYPE_JOB,
+            WORKLOAD_TYPE_CRONJOB,
+            WORKLOAD_TYPE_POD
+    );
 
 }
--- src/main/java/dev/nocalhost/plugin/intellij/utils/KubeResourceUtil.java
+++ src/main/java/dev/nocalhost/plugin/intellij/utils/KubeResourceUtil.java
@@ -8,13 +8,20 @@ import java.util.stream.Collectors;
 import dev.nocalhost.plugin.intellij.commands.data.kuberesource.Container;
 import dev.nocalhost.plugin.intellij.commands.data.kuberesource.KubeResource;
 
+import static dev.nocalhost.plugin.intellij.utils.Constants.WORKLOAD_TYPE_CRONJOB;
+import static dev.nocalhost.plugin.intellij.utils.Constants.WORKLOAD_TYPE_DAEMONSET;
+import static dev.nocalhost.plugin.intellij.utils.Constants.WORKLOAD_TYPE_DEPLOYMENT;
+import static dev.nocalhost.plugin.intellij.utils.Constants.WORKLOAD_TYPE_JOB;
+import static dev.nocalhost.plugin.intellij.utils.Constants.WORKLOAD_TYPE_POD;
+import static dev.nocalhost.plugin.intellij.utils.Constants.WORKLOAD_TYPE_STATEFULSET;
+
 public final class KubeResourceUtil {
     public static List<String> resolveContainers(KubeResource resource) {
         switch (resource.getKind().toLowerCase()) {
-            case "deployment":
-            case "daemonset":
-            case "statefulset":
-            case "job":
+            case WORKLOAD_TYPE_DEPLOYMENT:
+            case WORKLOAD_TYPE_DAEMONSET:
+            case WORKLOAD_TYPE_STATEFULSET:
+            case WORKLOAD_TYPE_JOB:
                 return resource
                         .getSpec()
                         .getTemplate()
@@ -23,7 +30,7 @@ public final class KubeResourceUtil {
                         .stream()
                         .map(Container::getName)
                         .collect(Collectors.toList());
-            case "cronjob":
+            case WORKLOAD_TYPE_CRONJOB:
                 return resource
                         .getSpec()
                         .getJobTemplate()
@@ -34,7 +41,7 @@ public final class KubeResourceUtil {
                         .stream()
                         .map(Container::getName)
                         .collect(Collectors.toList());
-            case "pod":
+            case WORKLOAD_TYPE_POD:
                 return resource
                         .getSpec()
                         .getContainers()
