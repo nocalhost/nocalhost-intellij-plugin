cea5a9d74ca9319e434378e41248a6e710ee0fa3
huangmingxin <huangmingxin@coding.net>
2021-07-20 17:53:51 +0800

    feature: skip ssh host key checking on git clone

--- src/main/java/dev/nocalhost/plugin/intellij/ui/action/namespace/InstallStandaloneApplicationAction.java
+++ src/main/java/dev/nocalhost/plugin/intellij/ui/action/namespace/InstallStandaloneApplicationAction.java
@@ -128,10 +128,7 @@ public class InstallStandaloneApplicationAction extends DumbAwareAction {
             try {
                 Path tempDir = Files.createTempDirectory("nocalhost-install-standalone-application-");
                 tempDir.toFile().deleteOnExit();
-                outputCapturedGitCommand.clone(tempDir.getParent(), gitUrl.get(), tempDir.getFileName().toString());
-                if (StringUtils.isNotEmpty(gitRef.get())) {
-                    outputCapturedGitCommand.checkout(tempDir, gitRef.get());
-                }
+                outputCapturedGitCommand.clone(tempDir, gitUrl.get(), gitRef.get());
                 localPath.set(tempDir);
                 resolveConfig();
             } catch (Exception e) {
