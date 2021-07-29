fc7e5fcbe212f3402073e21648892361c008fbe5
mingshun <mingshun@users.noreply.github.com>
2021-07-29 17:24:18 +0800

    refactor: git clone skip host key checking (#28)

--- src/main/java/dev/nocalhost/plugin/intellij/commands/GitCommand.java
+++ src/main/java/dev/nocalhost/plugin/intellij/commands/GitCommand.java
@@ -13,61 +13,33 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import dev.nocalhost.plugin.intellij.exception.NocalhostExecuteCmdException;
-import dev.nocalhost.plugin.intellij.exception.NocalhostGitCloneException;
 
 public class GitCommand {
     private static final String GIT_COMMAND = "git";
-    private static final Pattern HEAD_BRANCH_PATTERN = Pattern.compile("HEAD branch\\:\\s(.+)");
 
     public void clone(Path gitDir, String url) throws NocalhostExecuteCmdException, InterruptedException, IOException {
         clone(gitDir, url, null);
     }
 
     public void clone(Path gitDir, String url, String ref) throws IOException, NocalhostExecuteCmdException, InterruptedException {
-        Files.createDirectories(gitDir);
-
         String workTreePath = gitDir.toAbsolutePath().toString();
         String gitDirPath = gitDir.resolve(".git").toAbsolutePath().toString();
 
-        List<String> init = Lists.newArrayList(GIT_COMMAND, "--work-tree", workTreePath, "--git-dir", gitDirPath,
-                "init", ".");
-        execute(init);
+        List<String> clone = Lists.newArrayList(GIT_COMMAND, "clone", url, gitDir.toString(),
+                "--config", "core.sshCommand=ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no");
+        execute(clone);
 
-        List<String> addRemote = Lists.newArrayList(GIT_COMMAND, "--work-tree", workTreePath, "--git-dir", gitDirPath,
-                "remote", "add", "origin", url);
-        execute(addRemote);
-
-        List<String> configSshCommand = Lists.newArrayList(GIT_COMMAND, "--work-tree", workTreePath, "--git-dir", gitDirPath,
-                "config", "core.sshCommand", "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no");
-        execute(configSshCommand);
-
-        List<String> fetch = Lists.newArrayList(GIT_COMMAND, "--work-tree", workTreePath, "--git-dir", gitDirPath,
-                "fetch", "--prune", "--depth", "1", "origin");
-        execute(fetch);
-
-        if (!StringUtils.isNotEmpty(ref)) {
-            List<String> getHeadBranch = Lists.newArrayList(GIT_COMMAND, "--work-tree", workTreePath, "--git-dir", gitDirPath,
-                    "remote", "show", "origin");
-            String output = execute(getHeadBranch);
-            Matcher matcher = HEAD_BRANCH_PATTERN.matcher(output);
-            if (!matcher.find()) {
-                throw new NocalhostGitCloneException("No head branch found from repository " + url);
-            }
-            ref = matcher.group(1);
+        if (StringUtils.isNotEmpty(ref)) {
+            List<String> checkout = Lists.newArrayList(GIT_COMMAND, "--work-tree", workTreePath, "--git-dir", gitDirPath,
+                    "checkout", ref);
+            execute(checkout);
         }
-
-        List<String> checkout = Lists.newArrayList(GIT_COMMAND, "--work-tree", workTreePath, "--git-dir", gitDirPath,
-                "checkout", ref);
-        execute(checkout);
     }
 
     protected String execute(List<String> args) throws IOException, InterruptedException, NocalhostExecuteCmdException {
