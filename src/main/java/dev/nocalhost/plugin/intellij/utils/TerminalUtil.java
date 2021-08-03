472f10f35150bf601e3f79772deb399f71c06d21
huangmingxin <huangmingxin@coding.net>
2021-08-03 08:50:15 +0800

    fix: terminal working directory

--- src/main/java/dev/nocalhost/plugin/intellij/utils/TerminalUtil.java
+++ src/main/java/dev/nocalhost/plugin/intellij/utils/TerminalUtil.java
@@ -2,15 +2,15 @@ package dev.nocalhost.plugin.intellij.utils;
 
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.SystemProperties;
 
 import org.jetbrains.plugins.terminal.TerminalView;
 
 public final class TerminalUtil {
     public static void openTerminal(Project project, String title, GeneralCommandLine commandLine) {
         try {
-            TerminalView.getInstance(project).createLocalShellWidget(SystemProperties.getUserHome(),
-                    title, true).executeCommand(commandLine.getCommandLineString());
+            TerminalView.getInstance(project)
+                    .createLocalShellWidget(project.getBasePath(), title, true)
+                    .executeCommand(commandLine.getCommandLineString());
         } catch (Exception e) {
             ErrorUtil.dealWith(project, "Terminal open error",
                     "Error occurs while openning terminal", e);
