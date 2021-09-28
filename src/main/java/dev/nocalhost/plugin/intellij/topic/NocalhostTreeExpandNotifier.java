ba1a45a61349134c01db1d84bc462d9142ce4221
Raoh <232280074@qq.com>
2021-09-28 18:53:02 +0800

    - add --image when select image (#124)

--- src/main/java/dev/nocalhost/plugin/intellij/topic/NocalhostTreeExpandNotifier.java
+++ src/main/java/dev/nocalhost/plugin/intellij/topic/NocalhostTreeExpandNotifier.java
@@ -2,8 +2,6 @@ package dev.nocalhost.plugin.intellij.topic;
 
 import com.intellij.util.messages.Topic;
 
-import dev.nocalhost.plugin.intellij.settings.data.DevModeService;
-
 public interface NocalhostTreeExpandNotifier {
     @Topic.ProjectLevel
     Topic<NocalhostTreeExpandNotifier> NOCALHOST_TREE_EXPAND_NOTIFIER_TOPIC =
