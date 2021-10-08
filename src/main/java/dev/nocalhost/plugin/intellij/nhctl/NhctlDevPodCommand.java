e8aae61428e34a4276fb18e00e2e8e6ffe73c04c
Weijie <raoh.wu@gmail.com>
2021-10-08 11:22:51 +0800

    WIP: supports `duplicate` dev mode

--- src/main/java/dev/nocalhost/plugin/intellij/nhctl/NhctlDevPodCommand.java
+++ src/main/java/dev/nocalhost/plugin/intellij/nhctl/NhctlDevPodCommand.java
@@ -2,10 +2,8 @@ package dev.nocalhost.plugin.intellij.nhctl;
 
 import com.google.common.collect.Lists;
 
-import java.io.IOException;
 import java.util.List;
 
-import dev.nocalhost.plugin.intellij.exception.NocalhostExecuteCmdException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,11 +16,5 @@ public class NhctlDevPodCommand extends BaseCommand{
     protected List<String> compute() {
         List<String> args = Lists.newArrayList(getBinaryPath(), "dev", "pod", application);
         return fulfill(args);
-    }
-
-    @Override
-    public String execute() throws IOException, NocalhostExecuteCmdException, InterruptedException {
-        System.lineSeparator()
-        return super.execute().replace("\n", "");
     }
 }
