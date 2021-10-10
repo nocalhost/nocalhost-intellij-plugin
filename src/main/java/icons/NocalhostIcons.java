04d1920464a3bbf73472c10705a15cae2da84634
Weijie <raoh.wu@gmail.com>
2021-10-10 20:08:06 +0800

    WIP: supports `duplicate` dev mode

--- src/main/java/icons/NocalhostIcons.java
+++ src/main/java/icons/NocalhostIcons.java
@@ -19,7 +19,7 @@ public interface NocalhostIcons {
         Icon Unknown = IconLoader.getIcon("/icons/status_unknown.svg", NocalhostIcons.class);
         Icon Failed = IconLoader.getIcon("/icons/status-failed.svg", NocalhostIcons.class);
         Icon Loading = IconLoader.getIcon("/icons/loading.svg", NocalhostIcons.class);
-        Icon DevCopy = IconLoader.getIcon("/icons/dev_start_copy.svg", NocalhostIcons.class);
+        Icon DevCopy = IconLoader.getIcon("/icons/dev_copy.svg", NocalhostIcons.class);
         Icon DevCopyWithPortForwarding = IconLoader.getIcon("/icons/dev_port_forwarding.svg", NocalhostIcons.class);
         Icon DevStart = IconLoader.getIcon("/icons/dev_start.svg", NocalhostIcons.class);
         Icon DevEnd = IconLoader.getIcon("/icons/dev_end.svg", NocalhostIcons.class);
