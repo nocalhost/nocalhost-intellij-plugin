9d65893be2af38d9e3e81b356936d669ee8bc02f
Weijie <raoh.wu@gmail.com>
2021-12-22 12:59:30 +0800

    WIP: vpn-replace-mode
    Signed-off-by: Weijie <raoh.wu@gmail.com>

--- src/main/java/dev/nocalhost/plugin/intellij/nhctl/NhctlProxyCommand.java
+++ src/main/java/dev/nocalhost/plugin/intellij/nhctl/NhctlProxyCommand.java
@@ -1,13 +1,24 @@
 package dev.nocalhost.plugin.intellij.nhctl;
 
+import com.google.common.base.Charsets;
 import com.google.common.collect.Lists;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.SystemInfo;
 
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import dev.nocalhost.plugin.intellij.ui.dialog.SudoPasswordDialog;
+import dev.nocalhost.plugin.intellij.utils.ErrorUtil;
+import dev.nocalhost.plugin.intellij.utils.NhctlUtil;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -29,5 +40,45 @@ public class NhctlProxyCommand extends BaseCommand {
             args.add(workload);
         }
         return fulfill(args);
+    }
+
+    @Override
+    protected void consume(@NotNull Process process) {
+        if (SystemInfo.isWindows) {
+            return;
+        }
+
+        var output = "";
+        var reader = new InputStreamReader(process.getErrorStream(), Charsets.UTF_8);
+        try (var br = new BufferedReader(reader)) {
+            while ((output = br.readLine()) != null && isSudo(output)) {
+                ApplicationManager.getApplication().invokeLater(() -> {
+                    var dialog = new SudoPasswordDialog(project, NhctlUtil.binaryPath());
+                    if (dialog.showAndGet()) {
+                        inject(process, dialog.getPassword());
+                    }
+                });
+            }
+        } catch (IOException ex) {
+            // ignore
+        }
+    }
+
+    private boolean isSudo(String text) {
+        return StringUtils.contains(text, "Password:")
+                || StringUtils.contains(text, "[sudo] password for")
+                || StringUtils.contains(text, "Sorry, try again:");
+    }
+
+    private void inject(@NotNull Process process, @NotNull String password) {
+        ApplicationManager.getApplication().executeOnPooledThread(() -> {
+            var stream = process.getOutputStream();
+            try (stream) {
+                stream.write((password + "\n").getBytes(StandardCharsets.UTF_8));
+                stream.flush();
+            } catch (Exception ex) {
+                ErrorUtil.dealWith(project, "Failed to start proxy", "Error occurred while writing password to stdin", ex);
+            }
+        });
     }
 }
--- src/main/java/dev/nocalhost/plugin/intellij/ui/action/workload/ProxyConnectAction.java
+++ src/main/java/dev/nocalhost/plugin/intellij/ui/action/workload/ProxyConnectAction.java
@@ -4,17 +4,14 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.SystemInfo;
 
 import org.jetbrains.annotations.NotNull;
 
-import dev.nocalhost.plugin.intellij.nhctl.NhctlProxyCommand;
 import dev.nocalhost.plugin.intellij.ui.console.NocalhostConsoleManager;
-import dev.nocalhost.plugin.intellij.ui.dialog.SudoPasswordDialog;
 import dev.nocalhost.plugin.intellij.ui.tree.node.ResourceNode;
-import dev.nocalhost.plugin.intellij.utils.ErrorUtil;
+import dev.nocalhost.plugin.intellij.nhctl.NhctlProxyCommand;
 import dev.nocalhost.plugin.intellij.utils.KubeConfigUtil;
-import dev.nocalhost.plugin.intellij.utils.NhctlUtil;
+import dev.nocalhost.plugin.intellij.utils.ErrorUtil;
 import icons.NocalhostIcons;
 
 public class ProxyConnectAction extends DumbAwareAction {
@@ -29,17 +26,6 @@ public class ProxyConnectAction extends DumbAwareAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        if (SystemInfo.isWindows) {
-            execute(null);
-            return;
-        }
-        var dialog = new SudoPasswordDialog(project, NhctlUtil.binaryPath());
-        if (dialog.showAndGet()) {
-            execute(dialog.getPassword());
-        }
-    }
-
-    private void execute(String pwd) {
         NocalhostConsoleManager.activateOutputWindow(project);
         ApplicationManager.getApplication().executeOnPooledThread(() -> {
             try {
@@ -48,7 +34,7 @@ public class ProxyConnectAction extends DumbAwareAction {
                 cmd.setWorkload(node.getKubeResource().getKind() + "/" + node.resourceName());
                 cmd.setNamespace(node.getNamespaceNode().getNamespace());
                 cmd.setKubeConfig(KubeConfigUtil.kubeConfigPath(node.getClusterNode().getRawKubeConfig()));
-                cmd.execute(pwd);
+                cmd.execute();
             } catch (Exception ex) {
                 ErrorUtil.dealWith(project, "Failed to start proxy", "Error occurred while starting proxy.", ex);
             }
