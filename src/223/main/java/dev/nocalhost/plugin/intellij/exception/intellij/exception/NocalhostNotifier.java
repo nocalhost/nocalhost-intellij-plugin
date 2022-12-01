8c3de98bf13e7fad6740fa0a6e263fd45768ef46
AaronMegs <aaronmegs@hotmail.com>
2022-12-01 15:58:18 +0800

    support idea 223

---
+++ src/223/main/java/dev/nocalhost/plugin/intellij/exception/intellij/exception/NocalhostNotifier.java
@@ -0,0 +1,148 @@
+package src;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationListener;
+import com.intellij.notification.NotificationType;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.extensions.PluginId;
+import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.openapi.wm.ToolWindowManager;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.event.HyperlinkEvent;
+
+import dev.nocalhost.plugin.intellij.topic.NocalhostExceptionPrintNotifier;
+
+public class NocalhostNotifier {
+
+    public static final String NOCALHOST_NOTIFICATION_ID = "Nocalhost.Notification";
+    public static final String NOCALHOST_ERROR_NOTIFICATION_ID = "Nocalhost.Notification.Error";
+
+    public static final NotificationGroup NOCALHOST_NOTIFICATION =
+            NotificationGroup.create(
+                    "Nocalhost.Notification", NotificationDisplayType.BALLOON, false, "Nocalhost",
+                    "Nocalhost", PluginId.getId("dev.nocalhost.nocalhost-intellij-plugin"));
+    public static final NotificationGroup NOCALHOST_ERROR_NOTIFICATION =
+            NotificationGroup.create(
+                    "Nocalhost.Notification.Error", NotificationDisplayType.STICKY_BALLOON, true, "Nocalhost",
+                    "Nocalhost", PluginId.getId("dev.nocalhost.nocalhost-intellij-plugin"));
+
+    private final Project project;
+
+    public NocalhostNotifier(Project project) {
+        this.project = project;
+    }
+
+    public static NocalhostNotifier getInstance(Project project) {
+        return project.getService(NocalhostNotifier.class);
+    }
+
+    @NotNull
+    public Notification notify(@NotNull Notification notification) {
+        notification.notify(project);
+        return notification;
+    }
+
+    @NotNull
+    public Notification notifyError(@NlsContexts.NotificationTitle @NotNull String title,
+                                    @NlsContexts.NotificationContent @NotNull String message) {
+        return notify(NOCALHOST_ERROR_NOTIFICATION, NOCALHOST_ERROR_NOTIFICATION_ID, title, message, NotificationType.ERROR, null);
+    }
+
+    @NotNull
+    public Notification notifyError(@NlsContexts.NotificationTitle @NotNull String title,
+                                    @NlsContexts.NotificationContent @NotNull String message,
+                                    @NotNull String eMessage) {
+        String content = String.format("<html>%s <a href=\"nocalhost.show\">Show More</a></html>", message);
+        return notify(NOCALHOST_ERROR_NOTIFICATION, NOCALHOST_ERROR_NOTIFICATION_ID, title, content, NotificationType.ERROR, new NotificationListener.Adapter() {
+            @Override
+            protected void hyperlinkActivated(@NotNull Notification notification, @NotNull HyperlinkEvent e) {
+                ToolWindowManager.getInstance(project).getToolWindow("Nocalhost Console").activate(() -> {
+                    project.getMessageBus().syncPublisher(NocalhostExceptionPrintNotifier.NOCALHOST_EXCEPTION_PRINT_NOTIFIER_TOPIC)
+                           .action(title, message, eMessage);
+                });
+            }
+        });
+    }
+
+    public void notifyBinaryNotFound(String binary) {
+        String content = String.format("<html>%s binary not found. <a href=\"nocalhost.setting\">Setting</a></html>", binary);
+        notify(NOCALHOST_ERROR_NOTIFICATION, NOCALHOST_ERROR_NOTIFICATION_ID, "Nocalhost", content, NotificationType.ERROR, new NotificationListener.Adapter() {
+            @Override
+            protected void hyperlinkActivated(@NotNull Notification notification, @NotNull HyperlinkEvent e) {
+                ShowSettingsUtil showSettingsUtil = ApplicationManager.getApplication().getService(ShowSettingsUtil.class);
+                showSettingsUtil.showSettingsDialog(project, "Nocalhost");
+            }
+        });
+    }
+
+    public void notifyVersionTips() {
+        String content = "<html>nocalhost plugin need upgrade. <a href=\"nocalhost.setting\">upgrade plugin</a></html>";
+        notify(NOCALHOST_ERROR_NOTIFICATION, NOCALHOST_ERROR_NOTIFICATION_ID, "Nocalhost", content, NotificationType.ERROR, new NotificationListener.Adapter() {
+            @Override
+            protected void hyperlinkActivated(@NotNull Notification notification, @NotNull HyperlinkEvent e) {
+                BrowserUtil.browse("https://plugins.jetbrains.com/plugin/16058-nocalhost/versions");
+            }
+        });
+    }
+
+
+    @NotNull
+    public Notification notifyError(@NlsContexts.NotificationTitle @NotNull String title,
+                                    @NlsContexts.NotificationContent @NotNull String message,
+                                    @Nullable NotificationListener listener) {
+        return notify(NOCALHOST_ERROR_NOTIFICATION, NOCALHOST_ERROR_NOTIFICATION_ID, title, message, NotificationType.ERROR, listener);
+    }
+
+    @NotNull
+    public Notification notifySuccess(@NlsContexts.NotificationTitle @NotNull String title,
+                                      @NlsContexts.NotificationContent @NotNull String message) {
+        return notify(NOCALHOST_NOTIFICATION, NOCALHOST_NOTIFICATION_ID, title, message, NotificationType.INFORMATION, null);
+    }
+
+    @NotNull
+    public Notification notifySuccess(@NlsContexts.NotificationTitle @NotNull String title,
+                                      @NlsContexts.NotificationContent @NotNull String message,
+                                      @Nullable NotificationListener listener) {
+        return notify(NOCALHOST_NOTIFICATION, NOCALHOST_NOTIFICATION_ID, title, message, NotificationType.INFORMATION, listener);
+    }
+
+    @NotNull
+    private Notification notify(@NotNull NotificationGroup notificationGroup,
+                                @NonNls @Nullable String displayId,
+                                @NlsContexts.NotificationTitle @NotNull String title,
+                                @NlsContexts.NotificationContent @NotNull String message,
+                                @NotNull NotificationType type,
+                                @Nullable NotificationListener listener) {
+        Notification notification = createNotification(notificationGroup, displayId, title, message, type, listener);
+        return notify(notification);
+    }
+
+    private static Notification createNotification(@NotNull NotificationGroup notificationGroup,
+                                                   @NonNls @Nullable String displayId,
+                                                   @NlsContexts.NotificationTitle @NotNull String title,
+                                                   @NlsContexts.NotificationContent @NotNull String message,
+                                                   @NotNull NotificationType type,
+                                                   @Nullable NotificationListener listener) {
+
+        if (StringUtils.isBlank(message)) {
+            message = title;
+            title = "";
+        }
+        Notification notification = notificationGroup.createNotification(title, message, type);
+        if (listener != null) {
+            notification.setListener(listener);
+        }
+        notification.setDisplayId(StringUtils.trimToEmpty(displayId));
+        return notification;
+    }
+}
